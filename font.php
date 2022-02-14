3dce7fe2d6702912fd532e95434b9995135a6d3f
moodaeng16 <89384648+moodaeng16@users.noreply.github.com>
2022-02-14 17:43:02 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,31 @@
+<html>
+  <head>
+    <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+      @font-face{
+        font-family: myFont;
+        src: url(shining.woff);
+      }
+      @font-face{
+        font-family: myFont2;
+        src: url(MiPancakeBold_1.woff);
+        src: url(MiPancakeThin_1.woff);
+        src: url(MiPancake_1.woff);
+      }
+      #font1{
+        font-family: myFont;
+      }
+      #font2{
+        font-family: myFont2;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="font1">
+      <h1>น.ส. ศิราภรณ์ สิงห์ทา ม5/13เลขที่16</h1>
+    </div>
+    <div id="font2">
+      <h1>น.ส. ศิราภรณ์ สิงห์ทา ม5/13เลขที่16</h1>
+    </div>
+  </body>
+</html>
